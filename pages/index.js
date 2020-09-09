8fd85cc0166a32468eecd849101cdba872047246
JackWils91 <48407904+JackWils91@users.noreply.github.com>
2020-09-09 22:14:11 +0100

    Designing & building the dashboard (#3)
    * Firebase, Chakra UI, Absolute Imports
    
    * Designing & Building the Dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
 import Head from 'next/head';
 import { useAuth } from '@/lib/auth';
-import { Button, Heading, Text, Code } from '@chakra-ui/core';
+
+import { Button, Heading, Text, Code, Icon, Flex } from '@chakra-ui/core';
+
 
 const Home = () => {
   const auth = useAuth();
   return (
-    <div>
+
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
-      <main>
-        <Heading>Fast Feedback</Heading>
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
-        </Text>
 
-        {auth.user ? (
-          <Button onClick={() => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => auth.signinWithGithub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+      <Heading>Fast Feedback</Heading>
+      <Icon color="black" name="logo" size="64px" />
+
+      {auth.user ? (
+        <Button onClick={() => auth.signout()}>Sign Out</Button>
+      ) : (
+        <Button mt={4} size="sm" onClick={() => auth.signinWithGithub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
+
   );
 };
 
