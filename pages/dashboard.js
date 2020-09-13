9dbf2d21637dcbf4a8232ef92b06ac89cd3a2836
JackWils91 <jackdavidwilson@gmail.com>
2020-09-13 13:35:54 +0100

    creating feedback pages with SSG and styling

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,17 +1,34 @@
 import Head from 'next/head';
+import useSWR from 'swr';
 import { Button, Flex, Text, Code, Icon } from '@chakra-ui/core';
 
-import { useAuth } from '@/lib/auth';
 import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTable';
+import DashboardShell from '@/components/DashboardShell';
+import fetcher from '@/utils/fetcher';
+
+import { useAuth } from '@/lib/auth';
+import SiteTable from '@/components/SiteTable';
 
 const Dashboard = () => {
   const auth = useAuth();
+  const { data } = useSWR('/api/sites', fetcher);
 
-  if (!auth.user) {
-    return 'Loading...';
+  console.log(data);
+
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
   }
 
-  return <EmptyState />;
+  return (
+    <DashboardShell>
+      {data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  );
 };
 
 export default Dashboard;
